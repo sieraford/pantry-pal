0ebc7735324e7a5fdc37dd626ac54dba11a091b2
Carli Hudson <carlihudson@Carlis-MacBook-Pro.local>
2023-01-30 22:51:27 -0500

    finished item routes

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -58,4 +58,63 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// create a new food item
+router.post('/', async (req, res) => {
+  console.log("Item Post Route Start")
+  try {
+    const newItem = await Item.create(req.body, {
+      include: [
+        {
+          model: Category,
+          attributes: ["name"]
+      },
+        {
+          model: User,
+          through: UserItem, 
+        },
+      ],
+    });
+    console.log(newItem)
+    if (!newItem) {
+      res.status(404).json({ message: 'Please enter a food item' });
+      return;
+    }
+    res.status(200).json(newItem);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const itemToUpdate = await Item.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!itemToUpdate[0]) {
+      res.status(404).json({ message: 'No item with this id!' });
+      return;
+    }
+    res.status(200).json(itemToUpdate);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const itemToDelete = await Item.findByPk(req.params.id)
+  Item.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then(() => {
+    res.json(`Item has been removed!`)
+  })
+  .catch((err) => {
+    res.json(err);
+  })
+});
+
 module.exports = router;

9ea2b4b1a4ef1497f7be8e30832ea57b441a8262
Matthew Tobin <tobin.matthew94@gmail.com>
2023-01-30 19:50:29 -0800

    added homepage view page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,31 +1,31 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Item, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     // Get all projects and JOIN with user data
-//     const projectData = await Project.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
+router.get('/', withAuth, async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const itemData = await Item.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-//     // Serialize data so the template can read it
-//     const projects = projectData.map((project) => project.get({ plain: true }));
+    // Serialize data so the template can read it
+    const items = itemData.map((item) => item.get({ plain: true }));
 
-//     // Pass serialized data and session flag into template
-//     res.render('homepage', { 
-//       projects, 
-//       logged_in: req.session.logged_in 
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      items, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // router.get('/project/:id', async (req, res) => {
 //   try {
@@ -72,7 +72,7 @@ const withAuth = require('../utils/auth');
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/');
     return;
   }
 

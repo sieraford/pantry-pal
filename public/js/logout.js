6cab73d6c9c6b0d97eb270ad0da2fd173215f40d
klgibsonjr <klgibsonjr@gmail.com>
2023-02-02 21:57:46 -0800

    created landing page, create seperate login and signup pages

--- public/js/logout.js
+++ public/js/logout.js
@@ -5,7 +5,7 @@ const logout = async () => {
   });
 
   if (response.ok) {
-    document.location.replace('/');
+    document.location.replace('/landing');
   } else {
     alert(response.statusText);
   }
